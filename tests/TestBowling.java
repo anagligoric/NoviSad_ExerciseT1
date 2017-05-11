b60b3844243472e101687a5fcb9186c8caea78a4
somename <someemail>
2017-05-11 19:57:41 +0200

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -5,11 +5,15 @@ import org.junit.Test;
 public class TestBowling {
 
 	@Test
-	public void test_firstThrow5_secondThrow5_isSpare() {
+	public void test_firstThrow5_secondThrow5_isSpare() throws BowlingException{
 		Frame frame = new Frame(5,5);
 		assertTrue(frame.isSpare());
 		
 		//fail("Not yet implemented");
+	}
+	@Test public void test_firstThrow3_secondThrow5_isSpare() throws BowlingException {
+		Frame frame = new Frame(3,5);
+		assertFalse(frame.isSpare());
 	}
 
 }
